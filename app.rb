f5865998e0be13490f5f8a5c7d438e0614ae76cd
cgjohnson <chuckjohn@tamu.edu>
2013-11-26 17:00:50 -0600

    modify signin functionality

--- app.rb
+++ app.rb
@@ -6,9 +6,9 @@ require 'ohai'
 
 helpers do
 
-  def username
-    session[:identity] ? session[:identity] : 'Login!'
-  end
+#  def username
+#    session[:identity] ? session[:identity] : 'Login!'
+#  end
 
   def system
     ohai = Ohai::System.new
@@ -17,6 +17,18 @@ helpers do
   end
 end
 
+#before '/secure/*' do
+#  unless session{:username => :password}
+#    session[:previous_url] = request.path
+#    @error = 'Yo dude! You need to be logged in to get to' + request.path
+#    halt slim(:signin)
+#  end
+#end
+
 get '/' do
   slim :signin
+end
+
+get '/secure/bowser' do
+  slim :bowser
 end
