a042dc0549dc159ff32f8138044dfa8298fbe613
cgjohnson <chuckjohn@tamu.edu>
2013-11-26 16:39:05 -0600

    add ohai gem and helpers to app.rb

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem "rerun"
 gem 'rb-fsevent', '~> 0.9.1'
 gem 'rb-inotify', '~> 0.8.8'
 gem 'slim'
+gem 'ohai'
--- app.rb
+++ app.rb
@@ -2,6 +2,20 @@
 require 'rubygems'
 require 'sinatra'
 require 'slim'
+require 'ohai'
+
+helpers do
+
+  def username
+    session[:identity] ? session[:identity] : 'Login!'
+  end
+
+  def system
+    ohai = Ohai::System.new
+    ohai.all_plugins
+    ohai
+  end
+end
 
 get '/' do
   slim :signin
