056582fafad23a1be7ad0449b362a2e04830959a
cgjohnson <chuckjohn@tamu.edu>
2013-12-02 09:54:49 -0600

    comment logging in app.rb out

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'ohai'
 require 'sinatra/contrib'
 require 'yaml'
 
-config_file 'secure.yaml'
+config_file 'secure.yml'
 
 configure do
 # uncomment for use with vagrant
@@ -14,6 +14,10 @@ configure do
 end
 
 helpers do
+#  def username
+#    session[:identity] ? session[:identity] : 'Hello User'
+#  end
+
   def system
     ohai = Ohai::System.new
     ohai.all_plugins
@@ -23,9 +27,9 @@ end
 
 =begin
 before '/secure/*' do
-  unless session[:username, :password] == ["charlie", "texas"]
+  unless session[:identity] == 'charlie'
     session[:previous_url] = request.path
-    @error = 'Yo dude, you need to be logged in to get to ' + request.path
+    @error = 'You need to be logged in to get to ' + request.path
     halt slim(:login)
   end
 end
@@ -35,11 +39,22 @@ get '/' do
   slim :login
 end
 
-post '/login/attempt' do
-  session[:username, :password] = params['username', 'password']
-  go_back = session[:previous_url] || '/'
-  redirect to go_back
-end
+#post '/login/attempt' do
+#  if params['username']=='charlie'&&params['password']=='texas'
+#    session[:identity] = 'charlie'
+#    redirect to '/secure/bowser'
+#  else
+#    "Username or password incorrect"
+#  end
+#session[:identity] = params['username']
+#  go_back = session[:previous_url] || '/'
+#  redirect to go_back
+#end
+
+#get '/logout' do
+#  session.delete(:identity)
+#  erb "<div class='alert alert-message'>Logged out</div>"
+#end
 
 get '/secure/bowser' do
   slim :secure
