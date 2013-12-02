bcf83d824617a0decfa32a6f0d9a8c9dbad0cb62
cgjohnson <chuckjohn@tamu.edu>
2013-12-02 13:49:43 -0600

    sign in checks username and password against yaml

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ require 'ohai'
 require 'sinatra/contrib'
 require 'yaml'
 
-config_file 'secure.yml'
+config_file 'config.yml'
 
 configure do
 # uncomment for use with vagrant
@@ -14,10 +14,6 @@ configure do
 end
 
 helpers do
-#  def username
-#    session[:identity] ? session[:identity] : 'Hello User'
-#  end
-
   def system
     ohai = Ohai::System.new
     ohai.all_plugins
@@ -25,37 +21,34 @@ helpers do
   end
 end
 
-=begin
+#=begin
 before '/secure/*' do
-  unless session[:identity] == 'charlie'
+  unless session[:identity] == 'admin'
     session[:previous_url] = request.path
-    @error = 'You need to be logged in to get to ' + request.path
+    @error = 'You need to be logged in to access ' + request.path
     halt slim(:login)
   end
 end
-=end
+#=end
 
 get '/' do
   slim :login
 end
 
-#post '/login/attempt' do
-#  if params['username']=='charlie'&&params['password']=='texas'
-#    session[:identity] = 'charlie'
-#    redirect to '/secure/bowser'
-#  else
-#    "Username or password incorrect"
-#  end
-#session[:identity] = params['username']
-#  go_back = session[:previous_url] || '/'
-#  redirect to go_back
-#end
-
-#get '/logout' do
-#  session.delete(:identity)
-#  erb "<div class='alert alert-message'>Logged out</div>"
-#end
+post '/login/attempt' do
+  if params['password'] == settings.credentials['password'] && params['username'] == settings.credentials['username']
+    session[:identity] = 'admin'
+    redirect '/secure/bowser'
+  else
+    redirect '/'
+  end
+end
 
 get '/secure/bowser' do
   slim :secure
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged Out</div>"
 end
