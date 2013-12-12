0a71f5964207b3e33b16c1f3213cd77d2c6bdbe7
Charlie <chuckjohn@tamu.edu>
2013-12-12 20:41:58 +0000

    app.rb skips network formatting if inputs remain unchanged

--- app.rb
+++ app.rb
@@ -58,26 +58,50 @@ end
 
 post '/secure/bowser' do
 
-  puts "Your hostname is #{params[:hostname]}"
-  puts "Your ip address is #{params[:ip]}"
-  puts "Your interface is #{params[:interface]}"
-  puts "Netmask in cidr #{params[:cidr]}"
-  puts "Your netmask is #{params[:netmask]}"
-  puts "Your broadcast address is #{params[:broadcast]}"
-  puts "Your gateway address is #{params[:gateway]}"
-
-  hostname = `sudo su -c 'echo #{params[:hostname]} > /etc/hostname && hostname #{params[:hostname]}'`
+  unless `hostname`.chomp == "#{params[:hostname]}"
+    hostname = `sudo su -c 'echo #{params[:hostname]} > /etc/hostname && hostname #{params[:hostname]}'`
+    puts "Your hostname is #{params[:hostname]}"
+  else
+    puts "hostname input unchanged"
+  end
 
   old_ip = NetworkInterface.addresses('eth0')[2].first["addr"]
-  add_ip = `sudo ip addr add #{params[:ip]}/#{params[:cidr]} dev #{params[:interface]}`
-  delete_ip = `sudo ip addr delete #{old_ip}/#{params[:cidr]} dev #{params[:interface]}`
 
-  netmask = `sudo ifconfig #{params[:interface]} netmask #{params[:netmask]}`
+  unless old_ip == "#{params[:ip]}"
+    add_ip = `sudo ip addr add #{params[:ip]}/#{params[:cidr]} dev #{params[:interface]}`
+    delete_ip = `sudo ip addr delete #{old_ip}/#{params[:cidr]} dev #{params[:interface]}`
+    puts "Your ip address is #{params[:ip]}"
+    puts "Your interface is #{params[:interface]}"
+  else
+    puts "ip input unchanged"
+  end
 
-  permit = `sudo ip link set dev eth0`
-  gateway = `sudo ip route add default via #{params[:gateway]}`
+  old_net = NetworkInterface.addresses('eth0')[2].first["netmask"]
 
-  broadcast = `sudo ifconfig #{params[:interface]} broadcast #{params[:broadcast]}`
+  unless old_net == "#{params[:netmask]}"
+    netmask = `sudo ifconfig #{params[:interface]} netmask #{params[:netmask]}`
+    puts "Netmask in cidr #{params[:cidr]}"
+    puts "Your netmask is #{params[:netmask]}"
+  else
+    puts "netmask input unchanged"
+  end
+
+  unless gateway == "#{params[:gateway]}"
+    permit = `sudo ip link set dev eth0`
+    gateway = `sudo ip route add default via #{params[:gateway]}`
+    puts "Your gateway address is #{params[:gateway]}"
+  else
+    puts "gateway input unchanged"
+  end
+
+  old_broad = NetworkInterface.addresses('eth0')[2].first["broadcast"]
+
+  unless old_broad == "#{params[:broadcast]}"
+    broadcast = `sudo ifconfig #{params[:interface]} broadcast #{params[:broadcast]}`
+    puts "Your broadcast address is #{params[:broadcast]}"
+  else
+    puts "broadcast input unchanged"
+  end
 
   redirect '/secure/bowser'
 end
