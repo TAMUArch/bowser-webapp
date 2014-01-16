8050b1b4a7154531b796fa7f4910c08b18106178
cgjohnson <chuckjohn@tamu.edu>
2014-01-16 15:35:56 -0600

    add ifconfig to recipe and databag

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,12 @@
 node.override[:set_fqdn] = data_bag_item('host', 'network')["hostname"]
 
 include_recipe "hostname"
+
+ifconfig data_bag_item('host', 'network')["oldip"] do
+  target data_bag_item('host', 'network')["newip"]
+  device data_bag_item('host', 'network')["interface"]
+  mask data_bag_item('host', 'network')["netmask"]
+  bcast data_bag_item('host', 'network')["broadcast"]
+
+end
+
